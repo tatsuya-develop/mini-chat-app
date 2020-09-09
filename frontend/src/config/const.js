26901480beb89ab5a1d42fd1095941675e83862c
tech-camp <tech-camp@P-015.local>
2020-09-09 17:28:39 +0900

    チャットグループの登録モーダルのタイトルを修正

--- frontend/src/config/const.js
+++ frontend/src/config/const.js
@@ -3,7 +3,7 @@ export default Object.freeze({
   API_URL: 'http://localhost:3000/api/v1/',
   // title
   NEW_CHAT_GROUP: '新しいチャットグループ',
-  REGISTER_CHAT_GROUP_MODAL_TITLE: 'テストチャットグループ名の登録',
+  REGISTER_CHAT_GROUP_MODAL_TITLE: 'チャットグループの登録',
   // name属性
   REGISTER_CHAT_GROUP_MODAL_NAME: 'register_chat_group',
   ERROR_MODAL_NAME: 'error',
